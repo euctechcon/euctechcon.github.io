f62df419381b8c1f44d079d0c61e52462552524a
Ivan <ivandemes-lab@outlook.com>
2025-05-28 11:08:08 +0200

    Update

---
+++ collage/2024/imageList.js
@@ -0,0 +1,10 @@
+const imageList = [
+  "GQ_5EK5WkAESA4g.jfif",
+  "GQ_9XDTXYAAea57.jfif",
+  "GQ_UC6CXsAAv8Gu.jfif",
+  "GRAaiAVXgAEdaMI.jfif",
+  "GRAfLioWcAAAjj5.jfif",
+  "GRAN0kaXwAEXPUH.jfif",
+  "GRAqlVCWMAAajDg.jfif",
+  "GRATLf3WwAAy-NE.jfif"
+];
--- collage/collage-2024.js
+++ collage/collage-2024.js
@@ -5,7 +5,7 @@ const imageDir = path.join(__dirname, '../media/collage/2024');
 const outputPath = path.join(__dirname, '2024/imageList.js');
 
 const imageFiles = fs.readdirSync(imageDir)
-  .filter(file => /\.(jpe?g|png|gif)$/i.test(file));
+  .filter(file => /\.(jpe?g|jfif|png|gif)$/i.test(file));
 
 const jsContent = `const imageList = ${JSON.stringify(imageFiles, null, 2)};`;
 
